f17ab92cc131ea5b47eee4f98077af52bba4c0aa
Matheus Joranhezon <mjoranhezon@gmail.com>
2019-09-29 15:22:42 -0300

    Adding method for user to sign in

--- project/api/routes/auth.py
+++ project/api/routes/auth.py
@@ -4,15 +4,11 @@ from flask import jsonify
 from flask import request
 from project import db
 from project.api.models import User
-from project.api.schemas.auth import validate_signup_json
+from project.api.schemas.auth import validate_signup_json, validate_login_json
 auth_blueprint = Blueprint('auth_blueprint', __name__)
 
-
-"""
-Nickname, email, password, password confirmation
-"""
 @auth_blueprint.route('/sign-up', methods=['POST'])
-def sign_up_gateway():
+def sign_up():
     request_data = validate_signup_json(request.get_json())
 
     if not request_data['success']:
@@ -35,4 +31,29 @@ def sign_up_gateway():
         return jsonify({
             'message': 'An error occured, please try again'
         }), 400
-        
+
+@auth_blueprint.route('/sign-in', methods=['POST'])
+def sign_in():
+    request_data = validate_login_json(request.get_json())
+
+    if not request_data['success']:
+        return jsonify({
+            'message': 'Invalid Data'
+        }), 400
+    
+    json_data = request_data['user']
+    user_data = User.query.filter_by(email = json_data['email']).first()
+
+    if user_data is not None:
+        if user_data.password == json_data['password']:
+            return jsonify({
+                'message': 'Login was made successfully'
+            }), 200
+        else:
+            return jsonify({
+                'message': 'Passwords do not match'
+            }), 400
+    else:
+        return jsonify({
+            'message': 'User not registered on database'
+        }), 400
--- project/api/schemas/auth.py
+++ project/api/schemas/auth.py
@@ -22,9 +22,32 @@ signup_schema = {
     "required": ["email", "password", "name", "username"]
 }
 
+login_schema = {
+    "type": "object",
+    "properties": {
+        "email": {
+            "type": "string",
+            "format": "email"
+        },
+        "password": {
+            "type": "string"
+        }
+    },
+    "required": ["email", "password"]
+}
+
 def validate_signup_json(data):
     try:
         validate(data, signup_schema)
+    except ValidationError as e:
+        return {'success': False, 'message': e}
+    except SchemaError as e:
+        return {'success': False, 'message': e}
+    return {'success': True, 'user': data}
+
+def validate_login_json(data):
+    try:
+        validate(data, login_schema)
     except ValidationError as e:
         return {'success': False, 'message': e}
     except SchemaError as e:
